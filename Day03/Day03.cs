31f506ee7a9a086095ad51da44969d67eb39593c
David Darj <zid@alagazam.net>
2021-12-03 12:54:02 +0100

    Day 3 part 1 solved

---
+++ Day03/Day03.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace AoC2020
+{
+    public class Day03
+    {
+        public static Int64 Day03a(string[] input)
+        {
+            List<int> bits = new List<int>(input[0].Length);
+            foreach(var i in input[0]) bits.Add(0);
+
+            foreach (var i in input)
+            {
+                var bitNo = 0;
+                foreach(var bit in i)
+                {
+                    if (bit == '0')
+                        bits[bitNo]++;
+                    bitNo++;
+                }
+            }
+
+            var gamma = 0;
+            var epsilon = 0;
+            foreach(var bitCount in bits)
+            {
+                gamma = gamma * 2;
+                epsilon = epsilon * 2;
+                if (bitCount > input.Length / 2)
+                {
+                    gamma++;
+                }
+                else
+                {
+                    epsilon++;
+                }
+            }
+
+            return gamma*epsilon;
+        }
+
+        public static Int64 Day03b(string[] input)
+        {
+            return 0;
+        }
+
+
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("Day03.txt");
+            var sw = Stopwatch.StartNew();
+            Console.WriteLine("Day03a : {0}   Time: {1}", Day03a(lines), sw.ElapsedMilliseconds);
+            sw.Restart();
+            Console.WriteLine("Day03b : {0}   Time: {1}", Day03b(lines), sw.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ Day03/Day03Test.cs
@@ -0,0 +1,48 @@
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AoC2020
+{
+    public class Day03Test
+    {
+        string input =
+@"00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010";
+
+        [Fact]
+        public void Day03a()
+        {
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            Assert.Equal(198, Day03.Day03a(lines));
+        }
+
+
+        [Fact]
+        public void Day03b()
+        {
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            Assert.Equal(0, Day03.Day03b(lines));
+        }
+
+        public Day03Test(ITestOutputHelper output)
+        {
+            var converter = new AoCUtils.Converter(output);
+            Console.SetOut(converter);
+        }
+
+    }
+
+}
