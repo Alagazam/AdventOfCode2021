f80da4b68c57d3e7bdd4fa7651f16b9e19b57d78
David Darj <zid@alagazam.net>
2021-12-15 14:53:03 +0100

    Day 15 part 1 solved

---
+++ Day15/Day15.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace AoC2020
+{
+    public class Day15
+    {
+        struct Pair
+        {
+            public Pair(int xx, int yy) { x = xx; y = yy; }
+            public int x;
+            public int y;
+            public static Pair operator +(Pair a, Pair b) { return new Pair(a.x + b.x, a.y + b.y); }
+            public bool isValid(int xmax, int ymax) { return x >= 0 && x <= xmax && y >= 0 && y <= ymax; }
+        }
+        static int h(Pair a, Pair b) {  return Math.Abs(a.x - b.x + a.y - b.y); } // manhattan distance from a to b
+
+        static List<Pair> directions= new List<Pair> { new Pair(0,1), new Pair(1,0), new Pair(0,-1), new Pair(-1,0) };
+
+        public static int A_Star(int[,] grid)
+        {
+            var xmax = grid.GetLength(0) - 1;
+            var ymax = grid.GetLength(1) - 1;
+            var start = new Pair(0, 0);
+            var end = new Pair(xmax, ymax);
+
+            var openQueue = new PriorityQueue<Pair, int>();
+            openQueue.Enqueue( start, 0 );
+            var openSet = new Dictionary<Pair, int>();
+            openSet[start] = 0;
+
+            var cameFrom = new Dictionary<Pair, Pair>();
+
+            var gScore = new Dictionary<Pair, int>();
+            gScore[start] = 0;
+
+            var fScore = new Dictionary<Pair, int>();
+            fScore[start] = h(start, end);  
+
+            while (openQueue.Count > 0)
+            {
+                var current = openQueue.Dequeue();
+                if (current.Equals(end))
+                {
+                    break;
+                }
+                foreach(var dir in directions)
+                {
+                    var neighbor = current + dir;
+                    if (!neighbor.isValid(xmax, ymax)) continue;
+
+                    var tentative_gScore = gScore[current] + grid[neighbor.x, neighbor.y];
+                    if (!gScore.ContainsKey(neighbor) || tentative_gScore < gScore[neighbor])
+                    {
+                        cameFrom[neighbor] = current;
+                        gScore[neighbor] = tentative_gScore;
+                        fScore[neighbor] = tentative_gScore + h(neighbor, end);
+                        if (!openSet.ContainsKey(neighbor))
+                        {
+                            openSet[neighbor] = 0;
+                            openQueue.Enqueue(neighbor, fScore[neighbor]);
+                        }
+                    }
+                }
+            }
+            return fScore[end];
+        }
+
+        public static Int64 Day15a(string[] input)
+        {
+            var grid = new int[input.Length, input[0].Length];
+            int y = 0;
+            foreach(var row in input)
+            {
+                int x = 0;
+                foreach(var c in row)
+                {
+                    grid[x, y] = c - '0';
+                    x++;
+                }
+                y++;
+            }
+
+            return A_Star(grid);
+        }
+
+        public static Int64 Day15b(string[] input)
+        {
+            return 0;
+        }
+
+
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("Day15.txt");
+            var sw = Stopwatch.StartNew();
+            Console.WriteLine("Day15a : {0}   Time: {1}", Day15a(lines), sw.ElapsedMilliseconds);
+            sw.Restart();
+            Console.WriteLine("Day15b : {0}   Time: {1}", Day15b(lines), sw.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ Day15/Day15Test.cs
@@ -0,0 +1,46 @@
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AoC2020
+{
+    public class Day15Test
+    {
+        string input =
+@"1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581";
+
+        [Fact]
+        public void Day15a()
+        {
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            Assert.Equal(40, Day15.Day15a(lines));
+        }
+
+
+        [Fact]
+        public void Day15b()
+        {
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            Assert.Equal(0, Day15.Day15b(lines));
+        }
+
+        public Day15Test(ITestOutputHelper output)
+        {
+            var converter = new AoCUtils.Converter(output);
+            Console.SetOut(converter);
+        }
+
+    }
+
+}
